a7e6dd0ae5836a43ab2af769719bf2464bd95ba4
kathariakhila <kathariakhila@gmail.com>
2015-11-20 00:59:22 +0530

    Submit Commit

--- src/KthIndexFromEnd.cpp
+++ src/KthIndexFromEnd.cpp
@@ -11,6 +11,38 @@ ERROR CASES: Return '\0' for invalid inputs.
 NOTES:
 */
 
+int calcLen(char *str){
+
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return i;
+
+}
+
 char KthIndexFromEnd(char *str, int K) {
-	return '\0';
+	
+	int len;
+	int position;
+
+	if (!str)
+		return '\0';
+
+	if (K < 0)
+		return '\0';
+
+	len = calcLen(str);
+
+	if (K > len-1)
+		return '\0';
+
+	else{
+		position = len - K - 1;
+
+		return str[position];
+
+	}
+
 }
