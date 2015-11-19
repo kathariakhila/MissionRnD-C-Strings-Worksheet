be26c6ebbef3b4d7699424fca604686b0ce42299
kathariakhila <kathariakhila@gmail.com>
2015-11-20 00:47:54 +0530

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -18,8 +18,91 @@ NOTES: Don't create new string.
 */
 
 #include <stdio.h>
+#include<math.h>
+
+void reverse(char *str, int len)
+{
+	int i = 0, j = len - 1, temp;
+	while (i<j)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		i++; j--;
+	}
+}
+
+int intToStr(int number, char str[], int decimal)
+{
+	int i = 0;
+	while (number)
+	{
+		str[i++] = (number % 10) + '0';
+		number = number / 10;
+	}
+
+	while (i < decimal)
+		str[i++] = '0';
+
+	reverse(str, i);
+	str[i] = '\0';
+	return i;
+}
+
+
+void conversion(float n, char *str, int point)
+{
+	int integer;
+	int i;
+
+	float floating;
+
+	if (n < 0){
+		str[0] = '-';
+		integer = -n;
+		floating = n + integer;
+		floating = -floating;
+
+		printf("\n%d : %f", integer, floating);
+
+		i = intToStr(integer, str + 1, 0);
+
+		if (point != 0)
+		{
+			str[i + 1] = '.';
+
+			floating = floating * pow((double)10, point);
+
+			intToStr(floating, str + i + 2, point);
+		}
+
+	}
+
+	else{
+
+		integer = n;
+		floating = n - integer;
+
+		i = intToStr(integer, str, 0);
+
+
+		if (point != 0)
+		{
+			str[i] = '.';
+
+			floating = floating * pow((double)10, point);
+
+			intToStr(floating, str + i + 1, point);
+		}
+
+	}
+}
+
 
 
 void number_to_str(float number, char *str,int afterdecimal){
+
+
+	conversion(number,str, afterdecimal);
 	
 }
